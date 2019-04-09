29869df84f0cc5674bcc75a2b3bb85eb9f5d37b5
PH <penghao20170136@163.com>
2019-04-09 12:48:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,15 @@ from framework import Config
 import json 
 import numpy as np 
 import os 
-os.environ["CUDA_VISIBLE_DEVICES"] = '7'
+from optparse import OptionParser
+
+
+parser = OptionParser()
+parser.add_option('--gpu',dest='gpu',default = 7,help = 'gpu')
+parser.add_option('--chpt_index',dest='ckpt_index',default = 1, help = 'ckpt index')
+(options, args) =parser.parse_args()
+
+os.environ["CUDA_VISIBLE_DEVICES"] = options.gpu
 p2id = json.load(open("../data/p2id.json"))
 word2id = json.load(open('../data/word2id.json'))
 id2word = {}
@@ -14,10 +22,11 @@ for key in word2id:
 
 config = Config()
 test = Test(config, '../ckpt', id2word)
-test.init_test(Model(config))
+test.init_test(Model(config), options.ckpt_index)
 
 print('请输入:')
 line = ''
+is_continue = False
 while line != 'stop':
     line = input()
     pins = line.strip().split()
@@ -25,9 +34,13 @@ while line != 'stop':
     target_seq_len = [0]
     target_seq_len[0] = len(pins)
     for i, pin in enumerate(pins):
+        if pin not in p2id:
+            is_continue = True
+            break
         query[0][i] = p2id[pin]
-    # print(query)
-    # print(target_seq_len)
+    if is_continue:
+        is_continue = False
+        continue
     test.test_one_step(query, target_seq_len)
 
     
