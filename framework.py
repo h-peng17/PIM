257b1e5215a1b7b2805d43fd359ff51073e9d89c
PH <penghao20170136@163.com>
2019-04-09 10:59:00 +0800

    Update framework.py

--- framework.py
+++ framework.py
@@ -114,8 +114,7 @@ class Train():
         np.save('../data/loss.npy', np.array(Loss))
                 
 class Test():
-    def __init__(self, test_data_loader, config, ckpt_dir, id2word):
-        self.test_data_loader = test_data_loader
+    def __init__(self, config, ckpt_dir, id2word):
         self.config = config 
         self.ckpt_dir = ckpt_dir
         self.id2word = id2word
@@ -125,6 +124,8 @@ class Test():
         self.test_model = model
         self.test_model.cuda()
         self.test_model.eval()
+        path = os.path.join(self.ckpt_dir, self.config.model_name + '-' + str(5))
+        self.test_model.load_state_dict(torch.load(path))
     
     def to_val(self, x):
         return torch.from_numpy(x).to(torch.int64).cuda()
@@ -134,8 +135,8 @@ class Test():
     
     def convert2word(self, output, target_seq_len):
         '''
-            output: [batch, seq_len]
-            target_seq_len: [batch]
+            output: [1, seq_len]
+            target_seq_len: [1]
         '''
         out_file = ''
         for i in range(len(target_seq_len)):
@@ -144,27 +145,22 @@ class Test():
                 seq += self.id2word[output[i][j]] + ' '
             seq += '\r'
             out_file += seq
-        with open('output.txt', 'w') as f:
+        print(out_file)
+        with open('../data/output.txt', 'w') as f:
             f.write(out_file)
         print('done!')
 
-    def test_one_step(self):
-        batch = self.test_data_loader.next_batch()
-        self.test_model.embedding.query_seq = self.to_val(batch["query_seq"])
-        self.test_model.embedding.target_seq = self.target_seq(batch["query_seq"].shape()[0])
-        target_seq_len = batch["target_seq_len"].tolist()
+    def test_one_step(self, query_seq, target_seq_len):
+        self.test_model.embedding.query_seq = self.to_val(query_seq)
+        self.test_model.embedding.target_seq = self.target_seq(1)
 
-        output = self.test_model.test() #[batch, seq_len]
+        output = self.test_model.test() #[1, seq_len]
         output = np.array((output.cpu()).detach())
         output = output.tolist()
         self.convert2word(output, target_seq_len)
     
 
-config = Config()
-train_data_loader = Data_loader('train', config)
-train = Train(train_data_loader, config, '../ckpt')
-train.init_train(Model(config))
-train._train()
+
 
 
         
