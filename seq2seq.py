4e3e6d9b26fcb52e7d1c0343e20ef9ea0cdc0328
PH <penghao20170136@163.com>
2019-04-10 00:27:41 +0800

    Update seq2seq.py

--- seq2seq.py
+++ seq2seq.py
@@ -32,13 +32,14 @@ class Seq2seq(nn.Module):
         super(Seq2seq, self).__init__()
         self.input_size = config.embedding_size
         self.hidden_size = config.hidden_size
-        self.rnn_encoder = nn.GRU(input_size = self.hidden_size, hidden_size = self.hidden_size)
-        self.rnn_decoder = nn.GRU(input_size = self.hidden_size, hidden_size = self.hidden_size)
-        self.linear = nn.Linear(config.hidden_size, config.vacab_size + 3)
+        self.rnn_encoder = nn.GRU(input_size = self.hidden_size, hidden_size = self.hidden_size, num_layers = 4)
+        self.rnn_decoder = nn.GRU(input_size = self.hidden_size, hidden_size = self.hidden_size, num_layers = 4)
+        self.linear = nn.Linear(config.hidden_size * 2, config.vacab_size + 3)
         self.embed2input = nn.Linear(config.embedding_size, config.hidden_size)
         self.loss = nn.CrossEntropyLoss(reduce = False)
         self._init_weight()
         self.config = config
+        self.dropout = nn.Dropout(0.5)
         self.target_seq = None  # [batch_size, time_steps]
         self.loss_mask = None # [batch_size, time_steps]
         
@@ -78,7 +79,7 @@ class Seq2seq(nn.Module):
             input [batch_size, time_steps, hidden_size]
             函数返回:
             output [time_steps, batch_size, hidden_size], 每个时间步的输出
-            hidden [1, batch_size, hidden_size], 为最终的隐状态
+            hidden [4, batch_size, hidden_size], 为最终的隐状态
         '''
         # [B, T, E] -> [T, B, E]
         input = input.permute(1, 0, 2)
@@ -91,12 +92,12 @@ class Seq2seq(nn.Module):
             本函数为每一个时间步的decoder
             参数说明:
             input: [1, B, hidden_size], 为当前时间步的输入
-            hidden: [1, B, hidden_size], 为上一个时间步的隐状态(初始化为encoder的最后一个隐状态)
+            hidden: [4, B, hidden_size], 为上一个时间步的隐状态(初始化为encoder的最后一个隐状态)
             在训练时，input为当前的预测的单词的前一个单词，第一个为<SOS>
             在测试时，input为上一个时间步的预测输出
             函数返回值:
             output: [1, batch_size, hidden_size]
-            hidden: [1, batch_size, hidden_size]
+            hidden: [4, batch_size, hidden_size]
         '''
         # output = F.relu(input)
         # input = input.permute(1, 0, 2)
@@ -113,24 +114,39 @@ class Seq2seq(nn.Module):
             loss: 平均到每个instance的loss
         '''
         # 首先做一下映射
+        self.dropout(query_input)
+        self.dropout(target_input)
         query_input = self.embed2input(query_input)
         target_input = self.embed2input(target_input)
         loss = 0
-        _, encoder_hidden = self.encoder(query_input)
+        encoder_output, encoder_hidden = self.encoder(query_input)
+        encoder_output = encoder_output.permute(1, 0, 2) # [batch, time_step, hidden_size]
         decoder_input = torch.unsqueeze(target_input[:,0,:], 0) # init an input of [1, batch_size, hidden_size]
         decoder_hidden = encoder_hidden
         if is_training:
             for i in range(1, target_input.size()[1]):
                 decoder_output, decoder_hidden = self.decoder_step(decoder_input, decoder_hidden)
+                
+                decoder_output = decoder_output.permute(1, 2, 0) #[batch, hidden_size, 1]
+                weight = (torch.bmm(encoder_output, decoder_output)).permute(0, 2, 1) #[batch, time_step, 1] -> [batch, 1, timesteps]
+                context = (torch.bmm(weight, encoder_output)).permute(1, 0, 2) #
+                decoder_output = torch.cat((context, decoder_output.permute(2, 0, 1)), 2)
+                
                 loss += self.softmax_cross_entropyloss(decoder_output, self.target_seq[:, i-1], self.loss_mask[:, i-1])
                 decoder_input = torch.unsqueeze(target_input[:, i, :], 0) 
             return loss
         else:
-            output = [] # list of [1, batch_size, hidden_size]
+            output = [] # list of [1, batch_size, hidden_size * 2]
             for i in range(1, target_input.size()[1]):
                 decoder_output, decoder_hidden = self.decoder_step(decoder_input, decoder_hidden)
                 decoder_input = decoder_output
-                output.append(decoder_output)
+                # decode_output [1, batch, hidden_size] -> [batch, hidden_size, 1]
+                # encoder_output [batch, time_steps, hidden_size]
+                decoder_output = decoder_output.permute(1, 2, 0)
+                weight = (torch.bmm(encoder_output, decoder_output)).permute(0, 2, 1) #[batch, time_step, 1] -> [batch, 1, timesteps]
+                context = (torch.bmm(weight, encoder_output)).permute(1, 0, 2) #[batch, 1, hidden_size] -> [1, batch, hidden_size]
+                
+                output.append(torch.cat((context, decoder_input), 2))
             # [time_steps, batch_size, vacab_size] -> [batch_size, time_steps, vacab_size]
             output = self.linear(torch.cat(output, 0)).permute(1, 0, 2)
             return self.seq_output(output)
