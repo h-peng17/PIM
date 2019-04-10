21c5a1f0db35769ae48c0c61b0bf48e2bc7bae46
PH <penghao20170136@163.com>
2019-04-10 09:54:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -4,8 +4,14 @@ from framework import Model
 from framework import Config
 from data_loader import Data_loader
 import os 
-os.environ["CUDA_VISIBLE_DEVICES"] = '6'
+from optparse import OptionParser
 
+parser = OptionParser()
+parser.add_option('--gpu',dest='gpu',default = 7,help = 'gpu')
+parser.add_option('--ckpt_index',dest='ckpt_index',default = 1, help = 'ckpt index')
+(options, args) =parser.parse_args()
+
+os.environ["CUDA_VISIBLE_DEVICES"] = options.gpu
 config = Config()
 train_data_loader = Data_loader('train', config)
 train = Train(train_data_loader, config, '../ckpt_1')
