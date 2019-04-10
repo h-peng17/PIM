a552eddce87999db4983f1c7b38594f408f84652
PH <penghao20170136@163.com>
2019-04-10 19:44:03 +0800

    Update train.py

--- train.py
+++ train.py
@@ -9,11 +9,12 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option('--gpu',dest='gpu',default = 7,help = 'gpu')
 parser.add_option('--ckpt_index',dest='ckpt_index',default = 1, help = 'ckpt index')
+parser.add_option('--ckpt_dir', dest='ckpt_dir',default='ckpt',help='ckpt')
 (options, args) =parser.parse_args()
 
 os.environ["CUDA_VISIBLE_DEVICES"] = options.gpu
 config = Config()
 train_data_loader = Data_loader('train', config)
-train = Train(train_data_loader, config, '../ckpt_1')
+train = Train(train_data_loader, config, options.ckpt_dir)
 train.init_train(Model(config))
 train._train()
